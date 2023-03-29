63051c587736c82d0cf7148d38f78893d3b37ea5
Vitaly Kondratyuk <109025003+Vitalya-kon@users.noreply.github.com>
2023-03-29 16:33:48 +0500

    Add files via upload

--- plugins/datatables/js/datatables.init.js
+++ plugins/datatables/js/datatables.init.js
@@ -38,7 +38,23 @@
                 { title: 'Приходы' },
                 { title: 'Договоры' },
                 { title: '' },
-              ]
+              ],
+              createdRow: function ( row, data, index ) {
+                    $(row).addClass('selected')
+                } 
           });
-        });
+          table.on('click', 'tbody tr', function() {
+            var $row = table.row(this).nodes().to$();
+            var hasClass = $row.hasClass('selected');
+            if (hasClass) {
+                $row.removeClass('selected')
+            } else {
+                $row.addClass('selected')
+            }
+            })
+            
+            table.rows().every(function() {
+            this.nodes().to$().removeClass('selected')
+            });
+        });     
 })(jQuery);
